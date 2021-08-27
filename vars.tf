772b1ee720f72090486c0d3cd51bf81b063c2335
subtilf <felipe.subtil@outlook.com.br>
2021-08-27 16:46:26 -0300

    First versions of iam policy terraform module

---
+++ vars.tf
@@ -0,0 +1,16 @@
+variable iam_policy_name{
+    type = string
+}
+
+variable iam_policy_path{
+    type = string
+}
+
+variable iam_policy_desc{
+    default = "Default IAM policy."
+    type = string
+}
+
+variable iam_policy_json{
+    type = string
+}
